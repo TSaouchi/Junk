747b37bfed35b0f17bbc5f3677ddb8963837e723
SandBox <saouchi.toufik@gmail.com>
2025-01-22 18:44:54 +0100

    Update CoherenceExample.java

--- CoherenceExample.java
+++ CoherenceExample.java
@@ -2,10 +2,21 @@ package com.yourcompany;
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.NamedCache;
+import com.tangosol.net.InvocationService;
+import com.tangosol.net.QuietCacheFactory;
+import com.tangosol.net.Service;
+import com.tangosol.net.DefaultConfigurableCacheFactory;
+import com.tangosol.net.Coherence;
 
 public class CoherenceExample {
     public static void main(String[] args) {
         try {
+            // Specify the Coherence cluster address (IP and port of the Coherence server)
+            String clusterAddress = "your_ip_address:your_port_number";  // Replace with actual IP and port
+
+            // Set up the CacheFactory to connect to the Coherence cluster using the specified address
+            System.setProperty("tangosol.coherence.cluster", clusterAddress);
+
             // Start the Coherence cache (this assumes a Coherence cluster is running)
             String cacheName = "your_cache_name"; // Replace with your cache name
             NamedCache cache = CacheFactory.getCache(cacheName);
