bcd434b04633f308a08a4c5bd18cb77128cf9a08
SandBox <saouchi.toufik@gmail.com>
2025-02-19 09:11:21 +0100

    Delete trythis.ps1

--- trythis.ps1
+++
@@ -1,7 +0,0 @@
-$remoteHost = "host@remote"
-$password = "YourPassword"
-$secondaryCommand = "sudo su - otheraccount -c 'bash --noprofile --norc'"
-
-$plinkPath = "C:\Path\To\plink.exe"
-
-Start-Process -NoNewWindow -Wait -FilePath $plinkPath -ArgumentList "-batch -ssh $remoteHost -pw $password `"$secondaryCommand`""
