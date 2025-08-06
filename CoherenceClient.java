45734e362892c6bad10c007cce8b5e711b53efca
SandBox <saouchi.toufik@gmail.com>
2025-08-06 15:42:48 +0100

    Update CoherenceClient.java

--- CoherenceClient.java
+++ CoherenceClient.java
@@ -4,33 +4,51 @@
               xmlns="http://xmlns.oracle.com/coherence/coherence-cache-config"
               xsi:schemaLocation="http://xmlns.oracle.com/coherence/coherence-cache-config coherence-cache-config.xsd">
 
+    <!-- Map the cache name to a distributed scheme -->
     <caching-scheme-mapping>
         <cache-mapping>
             <cache-name>test-cache</cache-name>
-            <scheme-name>extend-scheme</scheme-name>
+            <scheme-name>distributed-scheme</scheme-name>
         </cache-mapping>
     </caching-scheme-mapping>
 
     <caching-schemes>
-        <extend-binary-scheme>
-            <scheme-name>extend-scheme</scheme-name>
-            <service-name>ExtendTcpCacheService</service-name>
-            <init-params>
-                <param>
-                    <name>port</name>
-                    <value>1408</value>
-                </param>
-            </init-params>
-        </extend-binary-scheme>
-    </caching-schemes>
 
+        <!-- Distributed caching scheme -->
+        <distributed-scheme>
+            <scheme-name>distributed-scheme</scheme-name>
+            <service-name>DistributedCache</service-name>
+            <backing-map-scheme>
+                <local-scheme/>
+            </backing-map-scheme>
+            <autostart>true</autostart>
+        </distributed-scheme>
+
+        <!-- Extend proxy so clients can connect -->
+        <proxy-scheme>
+            <scheme-name>extend-proxy</scheme-name>
+            <service-name>ExtendTcpProxyService</service-name>
+            <acceptor-config>
+                <tcp-acceptor>
+                    <local-address>
+                        <address>0.0.0.0</address>  <!-- Bind to all interfaces -->
+                        <port>9005</port>           <!-- Client will connect here -->
+                    </local-address>
+                </tcp-acceptor>
+            </acceptor-config>
+            <autostart>true</autostart>
+        </proxy-scheme>
+
+    </caching-schemes>
 </cache-config>
+
 ------------------------------------------ client side --------------------------
-  <?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <cache-config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
               xmlns="http://xmlns.oracle.com/coherence/coherence-cache-config"
               xsi:schemaLocation="http://xmlns.oracle.com/coherence/coherence-cache-config coherence-cache-config.xsd">
 
+    <!-- Map to use the remote Extend proxy -->
     <caching-scheme-mapping>
         <cache-mapping>
             <cache-name>test-cache</cache-name>
@@ -39,54 +57,48 @@
     </caching-scheme-mapping>
 
     <caching-schemes>
-        <extend-binary-scheme>
+        <remote-cache-scheme>
             <scheme-name>extend-scheme</scheme-name>
             <service-name>ExtendTcpCacheService</service-name>
             <initiator-config>
                 <tcp-initiator>
                     <remote-addresses>
                         <socket-address>
-                            <address>192.168.1.100</address>  <!-- Replace with your cluster's proxy IP -->
-                            <port>1408</port>                 <!-- Port must match server-side -->
+                            <address>192.168.1.100</address> <!-- IP of your Coherence server -->
+                            <port>9005</port>                <!-- Port used by Extend proxy -->
                         </socket-address>
                     </remote-addresses>
+                    <connect-timeout>5s</connect-timeout>   <!-- Optional: fail fast -->
                 </tcp-initiator>
             </initiator-config>
-        </extend-binary-scheme>
+        </remote-cache-scheme>
     </caching-schemes>
-
 </cache-config>
 
 
-  import com.tangosol.net.CacheFactory;
+import com.tangosol.net.CacheFactory;
 import com.tangosol.net.NamedCache;
 
 public class ExtendClient {
     public static void main(String[] args) {
-        // Tell Coherence to use the client-side config with Extend settings
         System.setProperty("tangosol.coherence.cacheconfig", "client-cache-config.xml");
+        System.setProperty("tangosol.coherence.cluster", "remote-cluster");
 
         try {
-            // Obtain remote cache reference via Extend
             NamedCache<String, String> cache = CacheFactory.getCache("test-cache");
 
-            // Put data into cache
             cache.put("hello", "world");
+            String value = cache.get("hello");
 
-            // Retrieve and print data
-            System.out.println("Cached Value for 'hello': " + cache.get("hello"));
-
-            // Print cache size (remote)
+            System.out.println("Fetched from remote cache: " + value);
             System.out.println("Cache size: " + cache.size());
 
-            // Properly release cache resources
             cache.release();
         } catch (Exception e) {
-            System.err.println("Failed to connect or operate on cache:");
+            System.err.println("❌ Failed to connect to remote Coherence cluster:");
             e.printStackTrace();
-            System.exit(1); // Crash as you requested if connection fails
+            System.exit(1);
         } finally {
-            // Ensure cache factory is shut down cleanly
             CacheFactory.shutdown();
         }
     }
