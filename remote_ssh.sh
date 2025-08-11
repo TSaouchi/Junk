7c732c1c597ad6fd655a3380dab5507df665be0f
SandBox <saouchi.toufik@gmail.com>
2025-08-11 12:47:25 +0100

    Update remote_ssh.sh

--- remote_ssh.sh
+++ remote_ssh.sh
@@ -1,39 +1,15 @@
-# Define clusters and command
-$cluster1 = "user@cluster1"
-$cluster2 = "user@cluster2"
-$remoteCmd = "myalias"
-
-# Start ssh-agent service if not running
-$sshAgentStatus = Get-Service ssh-agent -ErrorAction SilentlyContinue
-if ($sshAgentStatus.Status -ne 'Running') {
-    Start-Service ssh-agent
-}
-
-# Add SSH key to agent (adjust path to your private key)
-ssh-add $env:USERPROFILE\.ssh\id_ed25519
-
-# Run remote commands on both clusters
-ssh $cluster1 "bash -i -c '$remoteCmd'"
-ssh $cluster2 "bash -i -c '$remoteCmd'"
-
-
 #!/bin/bash
 
-# Clusters
 CLUSTER1="user@cluster1"
 CLUSTER2="user@cluster2"
-
-# The alias or command to run remotely
 REMOTE_CMD="myalias"
 
-# Make sure ssh-agent is running
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    eval "$(ssh-agent -s)"
-fi
+ssh "$CLUSTER1" bash -c "'
+    echo Running alias on Cluster 1
+    bash -i -c \"$REMOTE_CMD\" &
 
-# Add your ssh key (you’ll enter passphrase once here)
-ssh-add ~/.ssh/id_ed25519
+    echo Running alias on Cluster 2
+    ssh $CLUSTER2 bash -i -c \"$REMOTE_CMD\" &
 
-# Run commands on both clusters
-ssh "$CLUSTER1" "bash -i -c '$REMOTE_CMD'"
-ssh "$CLUSTER2" "bash -i -c '$REMOTE_CMD'"
+    wait
+'"
